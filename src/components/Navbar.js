c603600cea02a55a2de207a47150cb1e4a151889
AMATAKYVI <yviamatak123@gmail.com>
2022-10-26 00:28:07 -0400

    working on navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import { Box, Button, chakra, Text, useColorMode } from '@chakra-ui/react';
+import { Image as logoImage } from '../data/dummyData';
+import { Image } from '@chakra-ui/react';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{
+        py: 2,
+        px: {
+          base: '12px',
+          sm: '16px',
+          md: '20px',
+          lg: '24px',
+          xl: '28px',
+          '2xl': '32px',
+        },
+        bg: colorMode === 'dark' ? 'gray.200' : 'gray.50',
+      }}
+    >
+      <chakra.div>
+        {/* use image base on theme changing */}
+        <Image
+          src={
+            colorMode === 'dark' ? logoImage.logo.white : logoImage.logo.black
+          }
+          alt="Dan Abramov"
+          sx={{
+            boxSize: '50px',
+            borderRadius: '2xl',
+          }}
+        />
+      </chakra.div>
+      <chakra.div>
+        <chakra.div
+          sx={{
+            display: { base: 'none', sm: 'none', md: 'block' },
+            color: 'black',
+          }}
+        >
+          Welcome to CTX
+        </chakra.div>
+      </chakra.div>
+      <chakra.div
+        sx={{
+          color: colorMode === 'dark' ? 'black' : 'white',
+        }}
+      >
+        <Button
+          onClick={toggleColorMode}
+          sx={{
+            bg: colorMode === 'dark' ? 'white' : 'gray.700',
+            _hover: { bg: colorMode === 'dark' ? 'white' : 'gray.700' },
+          }}
+        >
+          Dark or Light
+        </Button>
+      </chakra.div>
+    </Box>
+  );
 }
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
